e4726dbeb523e124e62c5a2b0f71fb7e8b2c7083
d3c6e1 <dmytro.koval@nure.ua>
2020-06-22 16:19:14 +0300

    0.3.0

--- jobsearch/flutter_service_worker.js
+++ jobsearch/flutter_service_worker.js
@@ -5,7 +5,7 @@ const RESOURCES = {
 "assets/assets/images/no_photo_user.png": "ce14d64d9212c455e4495252453d3d59",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "304cd3eb7f059996871326d3169b91e1",
+"assets/LICENSE": "ddc5e3da2c4304a9d723f5f093e1f013",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "1e306f083913ea7909e74bcdaf7baf57",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/job_512.png": "af5ea36637c5381cded837492ef9b542",
 "index.html": "6ebe8be9543012680b28e7e68bab1d37",
 "/": "6ebe8be9543012680b28e7e68bab1d37",
-"main.dart.js": "77f286016cafebdf2904314b2c62fff9",
+"main.dart.js": "cc9dc4172b24a8c01215dd17a9ee3313",
 "manifest.json": "25a2647b789d138087684a82be01191f"
 };
 
